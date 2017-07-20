3df163f496ec2706a5bd5afec0d5b0894d39352c
RoryEnright <enright2062@gmail.com>
2017-07-20 12:02:45 -0400

    Fix serializer for mountain. Ember now gets hike IDs
    mountain serializer needed a defined :hikes attribute.

--- app/serializers/mountain_serializer.rb
+++ app/serializers/mountain_serializer.rb
@@ -1,3 +1,6 @@
 class MountainSerializer < ActiveModel::Serializer
-  attributes :id, :name, :height
+  attributes :id, :name, :height, :hikes
+  def hikes
+    object.hikes.pluck(:id)
+  end
 end
--- scripts/create-example.sh
+++ scripts/create-example.sh
@@ -6,7 +6,7 @@ curl "${API}${URL_PATH}" \
   --include \
   --request POST \
   --header "Content-Type: application/json" \
-  --header "Authorization: Token token=BAhJIiViYzc2MDYzZWExMjNiMDFkYzNmMWQ5ZTgzMDI4ZmVkMwY6BkVG--8ffe5fec8234e6c99e2fcacf887d0b5ead34e546" \
+  --header "Authorization: Token token=BAhJIiU1ZTljMjIxODllNWIzNDBmNzM4MTdmZGQ4YmMyOTkwMgY6BkVG--9cf03fd6727da23de0c9b735e2823d9aed212348" \
   --data '{
     "hike": {
       "when": "2016-05-07",
@@ -14,7 +14,7 @@ curl "${API}${URL_PATH}" \
       "duration": "2",
       "note": "Test Test!",
       "user_id": "1",
-      "mountain_id": "10"
+      "mountain_id": "3"
     }
   }'
 
